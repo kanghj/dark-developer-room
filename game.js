906d77e68d79d3c63445666a14e16f07faa2c65f
Low WeiLin <lwl1991@gmail.com>
2017-12-03 15:06:42 +0800

    Phase0 - vision quest

--- game.js
+++ game.js
@@ -3,8 +3,14 @@
 // The state of the entire world.
 // This should be all that's needed to save the state of the game.
 let world = {
-  phase: 1,
+  phase: 0,
   money: 1000,
+
+  companyInfo: {
+    name: undefined,
+    industry: undefined,
+    slogan: undefined
+  }
 
 };
 
@@ -25,10 +31,38 @@ function clearSavedGame() {
 let ui = {};
 
 $(() => {
-  ui.money = $('#part1-money');
+  ui.money = $('#phase1-money');
   gameLoop();
   setInterval(gameLoop, 1000)
 });
+
+function renderPhaseElements() {
+  $("*[class^='phase']").hide();
+  $(`.phase${world.phase}`).show();
+}
+
+function phase0SetCompanyInfo() {
+  world.companyInfo.name = $(".phase0 > #company-name").val();
+  world.companyInfo.industry = $(".phase0 > #company-industry").val();
+  world.companyInfo.slogan = $(".phase0 > #company-slogan").val();
+  world.phase = 1;
+}
+
+function phase0VisionQuest() {
+  $('#vision-quest').show();
+}
+
+function renderCompanyInfo() {
+  if (world.companyInfo.name === undefined) {
+    $("#company-info").hide();
+  } else {
+    $("#company-info > #name").html(world.companyInfo.name);
+    $("#company-info > #industry").html(world.companyInfo.industry);
+    $("#company-info > #slogan").html(world.companyInfo.slogan);
+    $("#company-info").show();
+  }
+}
+
 
 function gameLoop() {
   // save();
@@ -42,6 +76,8 @@ function step() {
 }
 
 function render() {
+  renderPhaseElements();
+  renderCompanyInfo();
   ui.money.html(`$${world.money}`);
 }
 
